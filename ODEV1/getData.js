94c6ad3cf6616c1820563a3a0ed23aaf4740d597
CodeBusch <turkbahri19@hotmail.com>
2021-11-10 21:29:28 +0300

    int

--- ODEV1/getData.js
+++ ODEV1/getData.js
@@ -8,8 +8,8 @@ const  getData = async(id) =>{
     else if (typeof id == "number" && id == parseInt(id)){
         const {data : users} = await axios.get("https://jsonplaceholder.typicode.com/users/"+ id);
         const {data : post} = await axios("https://jsonplaceholder.typicode.com/posts?userId="+ id);
-        arrayList.push(users) 
-        arrayList.push(post)
+        arrayList.push(users) //users added
+        arrayList.push(post) //post added
         return  arrayList.map((item)=>{console.log(item);})
     
     }
